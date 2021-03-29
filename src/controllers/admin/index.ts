c5a7ca2ca04ff6b1dea25b8de251d2cd3ae47e0c
Amarto Pramanik <pramanik@purdue.edu>
2021-03-29 18:49:45 -0400

    added endpoint to get all users and delete users

--- src/controllers/admin/index.ts
+++ src/controllers/admin/index.ts
@@ -1,3 +1,4 @@
 import { getAllClubRequests, postApproveClubRequest, postDenyClubRequest, deleteClub, getAllClubs, undeleteClub } from "./club";
+import { deleteUser, getAllUsers } from "./user";
 
-export { getAllClubRequests, postApproveClubRequest, postDenyClubRequest, deleteClub, getAllClubs, undeleteClub };
+export { getAllClubRequests, postApproveClubRequest, postDenyClubRequest, deleteClub, getAllClubs, undeleteClub, deleteUser, getAllUsers };
