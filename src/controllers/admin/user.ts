273005a207810fe64e93dae8793ad3f409b43827
Amarto Pramanik <pramanik@purdue.edu>
2021-03-29 19:20:35 -0400

    Fixed some bugs with delete user and added ban/unban user

--- src/controllers/admin/user.ts
+++ src/controllers/admin/user.ts
@@ -77,19 +77,181 @@ export const deleteUser = async (req: Request, res: Response): Promise<void> => 
 
         user.clubs.forEach(userClub => {
             Club.findOne({ _id: userClub.club._id })
-                .then(club => {
+                .then(async club => {
                     if (club) {
                         // eslint-disable-next-line @typescript-eslint/no-explicit-any
                         club.members = (club?.members as any[]).filter(item => { return !item.member.equals(user._id); });
                         // eslint-disable-next-line @typescript-eslint/no-explicit-any
                         club.joinRequests = (club?.joinRequests as any[]).filter(item => { return !item.user.equals(user._id); });
-                        club.save();
+                        await club.save();
+                    }
+                });
+        });
+
+        user.joinRequests.forEach(userClub => {
+            Club.findOne({ _id: userClub.club._id })
+                .then(async club => {
+                    if (club) {
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        club.members = (club?.members as any[]).filter(item => { return !item.member.equals(user._id); });
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        club.joinRequests = (club?.joinRequests as any[]).filter(item => { return !item.user.equals(user._id); });
+                        await club.save();
                     }
                 });
         });
 
         await user.delete().exec();
         res.status(200).json({ message: "Successfully deleted user" });
+        return;
+    } catch (err) {
+        logger.error(err);
+        res.status(500).json({ error: err });
+        return;
+    }
+};
+
+
+export const banUser = async (req: Request, res: Response): Promise<void> => {
+    const { error } = deleteUserSchema.validate(req.body); // uses the same schema as delete user
+
+    if (error) {
+        res.status(400).json({ "error": error.message });
+        logger.debug(error);
+        return;
+    }
+
+    const bannedUserId = req.body.id;
+    const userId = req.userId;
+
+    try {
+        const currUser = await User.findById(userId).exec();
+        if (currUser?.appRole != APP_ROLE.ADMIN) {
+            // the current user is not an admin
+            res.status(403).json({ error: "Please use an admin account" });
+            return;
+        }
+
+        const user = await User.findById(bannedUserId)
+            .populate({
+                path: "clubs",
+                populate: { path: "club" }
+            })
+            .populate({
+                path: "joinRequests",
+                populate: { path: "club" }
+            }).exec();
+
+        if (!user) {
+            res.status(400).json({ error: "User not found" });
+            return;
+        }
+
+        // delete the user from all their clubs
+        user.clubs.forEach(userClub => {
+            Club.findOne({ _id: userClub.club._id })
+                .then(async club => {
+                    if (club) {
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        club.members = (club?.members as any[]).filter(item => { return !item.member.equals(user._id); });
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        club.joinRequests = (club?.joinRequests as any[]).filter(item => { return !item.user.equals(user._id); });
+                        await club.save();
+                    }
+                });
+        });
+
+        user.joinRequests.forEach(userClub => {
+            Club.findOne({ _id: userClub.club._id })
+                .then(async club => {
+                    if (club) {
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        club.members = (club?.members as any[]).filter(item => { return !item.member.equals(user._id); });
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        club.joinRequests = (club?.joinRequests as any[]).filter(item => { return !item.user.equals(user._id); });
+                        await club.save();
+                    }
+                });
+        });
+
+        user.banned = true;
+        await user.save();
+        res.status(200).json({ message: "Successfully banned user" });
+        return;
+    } catch (err) {
+        logger.error(err);
+        res.status(500).json({ error: err });
+        return;
+    }
+};
+
+export const unbanUser = async (req: Request, res: Response): Promise<void> => {
+    const { error } = deleteUserSchema.validate(req.body); // uses the same schema as delete user
+
+    if (error) {
+        res.status(400).json({ "error": error.message });
+        logger.debug(error);
+        return;
+    }
+
+    const unbannedUserId = req.body.id;
+    const userId = req.userId;
+
+    try {
+        const currUser = await User.findById(userId).exec();
+        if (currUser?.appRole != APP_ROLE.ADMIN) {
+            // the current user is not an admin
+            res.status(403).json({ error: "Please use an admin account" });
+            return;
+        }
+
+        const user = await User.findById(unbannedUserId)
+            .populate({
+                path: "clubs",
+                populate: { path: "club" }
+            })
+            .populate({
+                path: "joinRequests",
+                populate: { path: "club" }
+            }).exec();
+
+        if (!user) {
+            res.status(400).json({ error: "User not found" });
+            return;
+        }
+
+        // this user is deleted from all their clubs, so add them back
+        user.clubs.forEach(userClub => {
+            Club.findOne({ _id: userClub.club._id })
+                .then(async club => {
+                    if (club) {
+                        club.members.push({
+                            member: user._id,
+                            role: userClub.role
+                        });
+                        await club.save();
+                    }
+                });
+        });
+
+        // also add back all their join requests
+        user.joinRequests.forEach(userClub => {
+            Club.findOne({ _id: userClub.club._id })
+                .then(async club => {
+                    if (club) {
+                        club.joinRequests.push({
+                            user: user._id,
+                            status: userClub.status,
+                            requestedAt: userClub.requestedAt
+                        });
+                        await club.save();
+                    }
+                });
+        });
+
+        user.banned = false;
+        await user.save();
+        res.status(200).json({ message: "Successfully unbanned user" });
         return;
     } catch (err) {
         logger.error(err);
