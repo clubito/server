c2f0366998422dc39a106faf591d38c6704567f2
Amarto Pramanik <pramanik@purdue.edu>
2021-03-30 19:34:17 -0400

    Fixed notifications

--- src/util/notifications.ts
+++ src/util/notifications.ts
@@ -38,10 +38,8 @@ export const sendNotification = async (userId: string, notification: INotificati
         // XXX: For now just send notifcations as we get them, but maybe in sprint 3, we can 
         // improve this and batch notifcations. Maybe queue up notifications, and send every 20 secs
         const tickets = await expo.sendPushNotificationsAsync(messages) as any;
-
         const receiptIds: any[] = [];
         for (const ticket of tickets) {
-            logger.debug(ticket);
             if (ticket.id) {
                 receiptIds.push(ticket.id);
             }
@@ -50,7 +48,6 @@ export const sendNotification = async (userId: string, notification: INotificati
         for (const receiptId in receipts) {
             const status = receipts[receiptId]?.status;
             const details = receipts[receiptId]?.details;
-            logger.debug(status as string);
             if (status === "ok") {
                 return Promise.resolve(true);
             } else {
@@ -58,9 +55,10 @@ export const sendNotification = async (userId: string, notification: INotificati
             }
         }
     } catch (err) {
+        logger.error(err);
         throw err;
     }
-    return Promise.resolve(false);
+    return Promise.resolve(true);
 };
 
 // Add a send push notification given the user id/message/data
