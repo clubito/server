c1b5f114120d6490ada2e52b84a25c7f9c8f21ee
Amarto Pramanik <pramanik@purdue.edu>
2021-03-30 21:26:20 -0400

    fix bug

--- src/util/notifications.ts
+++ src/util/notifications.ts
@@ -2,6 +2,7 @@ import User from "@models/User";
 import Expo, { ExpoPushMessage } from "expo-server-sdk";
 import { INotificationInterface } from "@models/Interfaces/INotificationInterface";
 import logger from "@logger";
+import Club from "@models/Club";
 
 const expo = new Expo();
 
@@ -39,7 +40,7 @@ export const sendNotificationToUser = async (userId: string, notification: INoti
         // improve this and batch notifcations. Maybe queue up notifications, and send every 20 secs
         const ticket = await (expo.sendPushNotificationsAsync(messages) as any)[0];
         const receiptIds: any[] = [];
-        if (ticket.id) {
+        if (ticket?.id) {
             receiptIds.push(ticket.id);
         }
         const receipt = await expo.getPushNotificationReceiptsAsync(receiptIds)[0];
@@ -56,4 +57,41 @@ export const sendNotificationToUser = async (userId: string, notification: INoti
     }
 };
 
-// Add a send push notification given the user id/message/data
+// export const sendNotificationToClub = async (clubId: string, notification: INotificationInterface): Promise<boolean> => {
+//     try {
+//         logger.info("Sending notification to club", clubId, notification);
+//         const club = await Club.find({ _id: clubId, "deleted.isDeleted": false }).exec();
+
+//         if (!club) {
+//             throw new Error("Club not found");
+//         }
+
+//         const messages: ExpoPushMessage[] = [];
+//         messages.push({
+//             to: user.pushToken,
+//             sound: "default",
+//             body: notification.body ?? "New Clubito notification!",
+//             data: notification.data,
+//             title: notification.title ?? "Clubito"
+//         });
+
+//         // XXX: For now just send notifcations as we get them, but maybe in sprint 3, we can 
+//         // improve this and batch notifcations. Maybe queue up notifications, and send every 20 secs
+//         const ticket = await (expo.sendPushNotificationsAsync(messages) as any)[0];
+//         const receiptIds: any[] = [];
+//         if (ticket.id) {
+//             receiptIds.push(ticket.id);
+//         }
+//         const receipt = await expo.getPushNotificationReceiptsAsync(receiptIds)[0];
+//         const status = receipt?.status;
+//         const details = receipt?.details;
+//         if (status === "ok") {
+//             return Promise.resolve(true);
+//         } else {
+//             throw new Error(`There was an error sending a notification: ${details}`);
+//         }
+//     } catch (err) {
+//         logger.error(err);
+//         throw err;
+//     }
+// };
